8ef2cb2a929266f1b97227ef2f3ab21d93012148
Michael Liao <michaell.liao44452@gmail.com>
2021-01-06 11:54:04 -0500

    Added pygame template

--- src/main.py
+++ src/main.py
@@ -1,6 +1,32 @@
+import pygame
+
 def main():
     """ Main program """
-    print("Hello World")
+    screenSize = (576, 1024)
+    
+    ## initializing game
+    pygame.init()
+
+    # creating the display window
+    screen = pygame.display.set_mode(screenSize)
+
+    # manages the frame rate 
+    clock = pygame.time.Clock()
+
+    while True:
+
+        # loop that runs continuously as events occur
+        for event in pygame.event.get():
+
+            # allows for quitting the game through window exit button
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+        # limiting the frame rate
+        clock.tick(120)
 
 if __name__ == "__main__":
     main()
