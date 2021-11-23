b4a79c79841ef23cace5096eacf65394555173fc
Michael Liao <michaell.liao44452@gmail.com>
2021-11-23 04:26:32 -0500

    added templates

---
+++ src/bird.py
@@ -0,0 +1,13 @@
+class Bird():
+
+    downflap = None
+    upflap = None
+    midflap = None
+    bird_frames = []
+    
+    def __init__(self, screen):
+        bird_img = None
+        current_frame = None
+        centerx_pos = None
+        centery_pos = None
+        pass
---
+++ src/pipes.py
@@ -0,0 +1,9 @@
+class PipeSet():
+    def __init__(self,topY_pos, bottomY_pos, x_pos):
+        self.topY_pos = topY_pos
+        self.bottomY_pos = bottomY_pos
+        self.x_pos = x_pos
+
+class Pipes():
+    def __init__():
+        list_pipes = []
