b675fe8af21aebffc808ca1daf674b41ea9ebb1f
User-12691296 <94016358+User-12691296@users.noreply.github.com>
2021-11-30 17:07:07 -0500

    Update pipes.py

--- src/pipes.py
+++ src/pipes.py
@@ -1,11 +1,20 @@
+import pygame
+
 class PipeSet():
-    def __init__(self,topY_pos, bottomY_pos, x_pos, screen):
-        self.screen = screen
-        self.topY_pos = topY_pos
-        self.bottomY_pos = bottomY_pos
+    def __init__(self, topY_pos, bottomY_pos, x_pos):
         self.x_pos = x_pos
 
+        #Defining the image :)
+
+        self.rectBottom = self.imageBottom.get_rect()
+        self.rectBottom.rect.midtop = (x_pos, topY_pos)
+        self.rectTop = self.imageTop.get_rect()
+        self.rectTop.rect.midbottom = (x_pos, bottomY_pos)
+        
+    def moveLeft(self, dis):
+        self.rectBottom.x -= dis
+        self.rectTop.x -= dis
+
 class Pipes():
-    def __init__(self,screen):
-        self.screen = screen
+    def __init__():
         list_pipes = []
