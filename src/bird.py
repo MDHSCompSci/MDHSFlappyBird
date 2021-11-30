a77339dae478ba212b04220acfe4ce8a5ce76202
Michael Liao <michaell.liao44452@gmail.com>
2021-11-30 16:42:44 -0500

    fixed some mistakes

--- src/bird.py
+++ src/bird.py
@@ -1,3 +1,5 @@
+
+
 class Bird():
 
     downflap = None
--- src/pipes.py
+++ src/pipes.py
@@ -1,9 +1,11 @@
 class PipeSet():
-    def __init__(self,topY_pos, bottomY_pos, x_pos):
+    def __init__(self,topY_pos, bottomY_pos, x_pos, screen):
+        self.screen = screen
         self.topY_pos = topY_pos
         self.bottomY_pos = bottomY_pos
         self.x_pos = x_pos
 
 class Pipes():
-    def __init__():
+    def __init__(self,screen):
+        self.screen = screen
         list_pipes = []
