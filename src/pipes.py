ef1d42f56fe6263642c63bf70f002e905f0b8bf6
User-12691296 <94016358+User-12691296@users.noreply.github.com>
2021-12-07 17:18:22 -0500

    Updated the motion config, to allow red pipes to exist

--- src/pipes.py
+++ src/pipes.py
@@ -13,6 +13,8 @@ class PipeSet():
             pipe_image = pygame.image.load(os.path.join(assets_path, "pipe-green.png"))
         elif pipe_type == "red":
             pipe_image = pygame.image.load(os.path.join(assets_path, "pipe-red.png"))
+            self._og_pos = (self.bottomY_pos + self.topY_pos)/2
+            self._motion_type = 0
         self.pipe_type = pipe_type
 
         # Defining the pipes' images, flipping top image
@@ -26,10 +28,25 @@ class PipeSet():
         self.rectTop = self.imageTop.get_rect()
         self.rectTop.rect.midbottom = (x_pos, bottomY_pos)
         
-    def move(self, dis):
+    def move_pipe(self, speedx):
         # Subtracting fromt the x value to move the pipes left
-        self.rectBottom.x -= dis
-        self.rectTop.x -= dis
+        self.rectBottom.x -= speedx
+        self.rectTop.x -= speedx
+
+        # Moving the pipes up and down
+        if self.pipe_type=="red":
+            speedy = 4
+            if self._motion_type == 0:
+                self.rectBottom.y -= speedy
+                self.rectTop.y -= speedy
+            elif self._motion_type == 1:
+                self.rectBottom.y += speedy
+                self.rectTop.y += speedy
+                
+            if (self.rectTop.y<self._og_pos-192):
+                self._motion_type = 1
+            elif (self.rectBottom.bottom>self._og_pos+192):
+                self._motion_type = 0
         
     def displayPipe(self, surface):
         # Blitting the two images at the rects' coordinates
