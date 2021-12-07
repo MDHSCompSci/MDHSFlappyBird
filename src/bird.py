502425145506f668664f1797bdf178153cd9aa16
Hong Yu <hongyu@ryerson.ca>
2021-12-07 16:23:50 -0500

    added getX, getY

--- src/bird.py
+++ src/bird.py
@@ -6,10 +6,16 @@ class Bird():
     upflap = None
     midflap = None
     bird_frames = []
-    
+
     def __init__(self, screen):
         bird_img = None
         current_frame = None
-        centerx_pos = None
-        centery_pos = None
+        self.centerx_pos = None
+        self.centery_pos = None
         pass
+    
+    def getX(self):
+        return self.centerx_pos
+    
+    def getY(self):
+        return self.centery_pos
