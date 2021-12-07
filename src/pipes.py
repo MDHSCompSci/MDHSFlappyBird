e78bb3c91af4c756f086faf2965c975009f56624
User-12691296 <94016358+User-12691296@users.noreply.github.com>
2021-12-07 15:52:29 -0500

    Update pipes.py

--- src/pipes.py
+++ src/pipes.py
@@ -1,14 +1,19 @@
-import pygame
+import pygame, os
+
+# Defining the assets path, the file path where all the images are stored
+assets_path = os.path.join(os.path.split(os.getcwd())[0], "FlappyBird_Assets")
+
 
 class PipeSet():
-    def __init__(self, topY_pos, bottomY_pos, x_pos):
+    def __init__(self, topY_pos, bottomY_pos, x_pos, pipe_type="green"):
         self.x_pos = x_pos
 
-        # Defining the assets path
-        assets_path = os.path.join(os.path.split(os.getcwd())[0], "FlappyBird_Assets")
-
-        # Importing pipe image
-        pipe_image = pygame.image.load(os.path.join(assets_path, "pipe-green.png"))
+        # Importing pipe image, based on the pipe type
+        if pipe_type == "green":
+            pipe_image = pygame.image.load(os.path.join(assets_path, "pipe-green.png"))
+        elif pipe_type == "red":
+            pipe_image = pygame.image.load(os.path.join(assets_path, "pipe-red.png"))
+        self.pipe_type = pipe_type
 
         # Defining the pipes' images, flipping top image
         self.imageBottom = (pipe_image)
@@ -21,7 +26,7 @@ class PipeSet():
         self.rectTop = self.imageTop.get_rect()
         self.rectTop.rect.midbottom = (x_pos, bottomY_pos)
         
-    def moveLeft(self, dis):
+    def move(self, dis):
         # Subtracting fromt the x value to move the pipes left
         self.rectBottom.x -= dis
         self.rectTop.x -= dis
